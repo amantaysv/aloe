6cc081c4810e016b9a4eb2873ac3ff3bed1575c8
Amantay <amantay.sv@gmail.com>
2026-06-29 05:21:25 +0600

    refactor: reorganize layout structure in HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -40,37 +40,44 @@ export default async function HomePage() {
   }));
 
   return (
-    <MainContainer>
+    <>
       <BannerCarousel banners={banners} />
-      {popular.total > 0 && (
-        <ProductCarousel
-          title="Популярные товары"
-          href="/popular"
-          products={popular.products}
-          totalCount={popular.total}
-        />
-      )}
-      {newest.total > 0 && (
-        <ProductCarousel title="Новинки" href="/new" products={newest.products} totalCount={newest.total} />
-      )}
-      {onSale.total > 0 && (
-        <ProductCarousel title="Акции" href="/sale" products={onSale.products} totalCount={onSale.total} />
-      )}
-      {discounted.total > 0 && (
-        <ProductCarousel title="Скидки" href="/discount" products={discounted.products} totalCount={discounted.total} />
-      )}
-      {categoryProducts.map(
-        ({ cat, products, total }) =>
-          total > 0 && (
-            <ProductCarousel
-              key={cat.id}
-              title={cat.name}
-              href={`/catalog/${cat.slug}`}
-              products={products}
-              totalCount={total}
-            />
-          ),
-      )}
-    </MainContainer>
+      <MainContainer>
+        {popular.total > 0 && (
+          <ProductCarousel
+            title="Популярные товары"
+            href="/popular"
+            products={popular.products}
+            totalCount={popular.total}
+          />
+        )}
+        {newest.total > 0 && (
+          <ProductCarousel title="Новинки" href="/new" products={newest.products} totalCount={newest.total} />
+        )}
+        {onSale.total > 0 && (
+          <ProductCarousel title="Акции" href="/sale" products={onSale.products} totalCount={onSale.total} />
+        )}
+        {discounted.total > 0 && (
+          <ProductCarousel
+            title="Скидки"
+            href="/discount"
+            products={discounted.products}
+            totalCount={discounted.total}
+          />
+        )}
+        {categoryProducts.map(
+          ({ cat, products, total }) =>
+            total > 0 && (
+              <ProductCarousel
+                key={cat.id}
+                title={cat.name}
+                href={`/catalog/${cat.slug}`}
+                products={products}
+                totalCount={total}
+              />
+            ),
+        )}
+      </MainContainer>
+    </>
   );
 }
