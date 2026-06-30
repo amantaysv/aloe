df87b2a873c06ed0b4af77d40ca2841518285b6d
Amantay <amantay.sv@gmail.com>
2026-07-01 02:44:34 +0600

    refactor: comment out unused image configuration options in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ["image/webp"],
-    minimumCacheTTL: 2678400,
-    qualities: [75],
-    remotePatterns: [
-      { protocol: "https", hostname: "aloe.kg" },
-      { protocol: "https", hostname: "dnlburbuchxzxdmhuczu.supabase.co" },
-    ],
+    // formats: ["image/webp"],
+    // minimumCacheTTL: 2678400,
+    // qualities: [75],
+    // remotePatterns: [
+    //   { protocol: "https", hostname: "aloe.kg" },
+    //   { protocol: "https", hostname: "dnlburbuchxzxdmhuczu.supabase.co" },
+    // ],
+    unoptimized: true,
   },
   devIndicators: false,
 };
